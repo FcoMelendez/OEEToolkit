93b8cc2fb0cf5f9b9be7d2c95110513dbb7c46c2
Francisco <fco.melendez.f@gmail.com>
2022-04-06 13:57:57 +0200

    Changing project name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme_file:
 requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
-    name="notebookc",
+    name="OEEToolkit",
     version="0.0.2",
     author="Jeff Hale",
     author_email="fco.melendez.f@gmail.com",
